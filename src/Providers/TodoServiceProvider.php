b571dfcaddb68fb80bf61fb0e9e63eba2057214d
helal <helal@logicsoftbd.com>
2025-05-05 17:55:13 +0600

    path fix

--- src/Providers/TodoServiceProvider.php
+++ src/Providers/TodoServiceProvider.php
@@ -13,8 +13,8 @@ class TodoServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
-        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'todo');
+        $this->loadRoutesFrom(__DIR__.'../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'../database/migrations');
+        $this->loadViewsFrom(__DIR__.'../resources/views', 'todo');
     }
 }
